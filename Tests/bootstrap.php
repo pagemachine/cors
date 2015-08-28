b3cf0dfcf9a0cd1cc63fc25f92a1bd00498d3ec4
Mathias Brodala <mbrodala@pagemachine.de>
2015-08-28 16:45:49 +0200

    [TASK] Allow test execution from any path

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -5,7 +5,7 @@ if (!file_exists(__DIR__ . '/../.vendor/autoload.php')) {
   throw new \RuntimeException('Could not find .vendor/autoload.php, make sure you ran composer.');
 }
 
-define('PATH_thisScript', realpath('.vendor/typo3/cms/typo3/index.php'));
+define('PATH_thisScript', realpath(__DIR__ . '/../.vendor/typo3/cms/typo3/index.php'));
 define('TYPO3_MODE', 'BE');
 putenv('TYPO3_CONTEXT=Testing');
 
